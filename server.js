60bde17c55e668d92c1ed48e377113de21d22e6b
PriyadarshiniArunachalam <30755514+PriyadarshiniArunachalam@users.noreply.github.com>
2017-08-06 19:51:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+  "article-one":{
+      "title":'Displaying first article.',
+      "heading":'Article-one',
+      "content":'<p> This is article one</p> '
+  },
+  "article-two":{
+      "title":'Displaying first article.',
+      "heading":'Article-one',
+      "content":'<p> This is article one</p> '
+      
+  },
+  "article-three":{
+      "title":'Displaying first article.',
+      "heading":'Article-one',
+      "content":'<p> This is article one</p> '
+      
+  }
+};
+
+function htmlElement(articleData){
+    var title=articleData.title;
+    var heading=articleData.heading;
+    var content=articleData.content;
+var renderedHTML=`<!doctype html>
+<html>
+    <head>
+        <tiltle>
+            ${title}
+        </tiltle>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="myStyle">
+            <marquee>${heading}</marquee>
+        </div>
+        <div class="center">
+            <img src="/ui/madi.png" class="img-medium"/>
+        </div>
+        <br>
+        <div class="center text-big bold">
+          ${content}
+        </div>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+    </body>
+</html>`;
+return renderedHTML;
+}
+
+app.get('\:articleName',function(req,res){
+    var articleName=req.params.articleName;
+    res.send(htmlElement(articles[articleName]));
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
